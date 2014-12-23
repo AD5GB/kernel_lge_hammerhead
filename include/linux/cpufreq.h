1de86d35195f390512d316e1314b12d51fea1ea5
Randy DuCharme <radio.ad5gb@gmail.com>
2014-12-23 11:19:41 -0600

    Added new governors to the wicked config.
    Signed-off-by: Randy DuCharme <radio.ad5gb@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -375,8 +375,6 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
-<<<<<<< HEAD
-=======
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
@@ -397,7 +395,6 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
 extern struct cpufreq_governor cpufreq_gov_wheatley;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_wheatley)
->>>>>>> 758a162... Added a metric shit ton of govs and scheds
 #endif
 
 
